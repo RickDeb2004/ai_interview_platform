c2f2b0d8cb1e0d55fe3b8fa18c172f5c95915e27
RickDeb2004 <debanjanrick04@gmail.com>
2025-05-22 20:42:20 +0530

    resolved

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -23,25 +23,4 @@ function initFirebaseAdmin() {
   };
 }
 
-// export const { auth, db } = initFirebaseAdmin();
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCOuEiU4NuCakRM6wZij78TSHhl9LG4MOU",
-//   authDomain: "interview-44be8.firebaseapp.com",
-//   projectId: "interview-44be8",
-//   storageBucket: "interview-44be8.firebasestorage.app",
-//   messagingSenderId: "611773512278",
-//   appId: "1:611773512278:web:276bc4132011db7b01d9f7",
-//   measurementId: "G-BZ2PWZTLT5"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+export const { auth, db } = initFirebaseAdmin();
